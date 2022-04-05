e2160f1de4dd6c4d1a030c59d6318a18e375b452
Allison Bohannon <akboh.github@gmail.com>
2022-04-05 14:44:17 -0700

    commit tree 3744d80b40da73909ebc980c9648390cf242a252

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(loc) {
+    return Math.abs(loc - 42)
+};
+
+function distanceFromHqInFeet(loc) {
+    return distanceFromHqInBlocks(loc) * 264
+};
+
+function distanceTravelledInFeet(start,end) {
+    return Math.abs(end-start) * 264
+};
+
+function calculatesFarePrice(start,end) {
+    const distance = distanceTravelledInFeet(start,end)
+    if (distance > 2500) {
+        return 'cannot travel that far';
+    } else if (distance <= 2000) {
+        return Math.max(0, .02 * (distance - 400))
+    } else {
+        return 25
+    }
+}
